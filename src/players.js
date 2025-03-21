0a80a0a2bc4200579bd84af229ff4b12436cb398
BENIE SYLVESTRE <sylvestrebenie7829@gmail.com>
2025-03-21 01:06:09 +0000

    feat: structuration du projet et installation des dependences

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentin",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al-Nassr",
+    nationality: "Portugais",
+    jerseyNumber: 7,
+    age: 39,
+    imageUrl: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "PSG",
+    nationality: "Français",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Kevin De Bruyne",
+    team: "Manchester City",
+    nationality: "Belge",
+    jerseyNumber: 17,
+    age: 32,
+    imageUrl: "https://via.placeholder.com/150"
+  }
+];
+
+export default players;
